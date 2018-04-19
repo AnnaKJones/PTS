5450e0a32f6c82c0f18aa3d17c040715391f0128
herondc <32229486+herondc@users.noreply.github.com>
2018-04-19 08:25:28 -0500

    Update ptsother.py
    second commit edit session

--- ptsother.py
+++ ptsother.py
@@ -29,25 +29,45 @@ class course:
         self.title = title
     def __repr__(self):
         return (self.title)    
-class createsession(tutor,user):     
-    def __init__(self,date,place="UWI"):
-        super(createsession, self).__init__(n="",a,c,t="tutor")
+class createsession(object):     
+    def __init__(self,id,course,time,date,location,tutor,status,place="UWI"):
+        self.id = id
+        self.course = course
+        self.time = time
         self.date = date
+        self.location = location
+        self.tutor = tutor
         self.place = place
-    def get_date(self):
+        self.status = status
+    def changetime(self, newtime):
+        self.time = newtime
+        return self.time
+    def changedate(self, newdate):
+        self.date = newdate
         return self.date
-    def set_date(self):
-        self.date = date
+    def changelocation(self, newlocation):
+        self.location = newlocation
+        return self.location
+    def changestatus(self, newstatus):
+        self.status = newstatus
+        return self.status
     def __repr__(self):
-        return (self.date+"\n"+self.place)
-class picksession(student):
-    def __init__(self,place="UWI"):
-        self.place = place
- 
+        return (self.id+"\n"+self.date+"\n"+self.place)
+class picksession(createsession):
+    def __init__(self,id,course,time,date,location,tutor,student,status="Engaged",place="UWI"):
+        super(picksession, self).__init__(id,course,time,date,location,tutor,status,place="UWI")
+        self.student = student
+    def changestat(self, newstatus):
+        self.status = newstatus
+        return self.status    
+    def __repr__(self):
+        return (self.course+"\n"+self.date+"\n"+self.place)
+    
 tutor1 = tutor("mark small",22,"001")
 tutor2 = tutor("ren batts",21,"002")
 student1 = student("will breks", 19,"003")
 student2 = student("ben smith",18,"004")
 course1 = course("Physics of the Human Body")
 course2 = course("Web Development II")
-session1 = createsession("feb12")
+session1 = createsession("s001","course1","5pm","feb21","room1","tutor1","Available")
+sessionpick1 = picksession("s001","course1","5pm","feb21","room1","tutor1","student1")
