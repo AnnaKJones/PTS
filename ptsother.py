4ee9867bf8ed7fc9087a5e42cf066dbfdda8ce73
herondc <32229486+herondc@users.noreply.github.com>
2018-04-19 08:39:41 -0500

    Update ptsother.py
    third commit review class object created

--- ptsother.py
+++ ptsother.py
@@ -57,12 +57,21 @@ class picksession(createsession):
     def __init__(self,id,course,time,date,location,tutor,student,status="Engaged",place="UWI"):
         super(picksession, self).__init__(id,course,time,date,location,tutor,status,place="UWI")
         self.student = student
-    def changestat(self, newstatus):
+    def changestatus(self, newstatus):
         self.status = newstatus
         return self.status    
     def __repr__(self):
         return (self.course+"\n"+self.date+"\n"+self.place)
-    
+class review(object):
+    def __init__(self,id,time,date,name,info):
+        self.id = id
+        self.time = time
+        self.date = date
+        self.name = name
+        self.info = info
+    def __repr__(self):
+        return (self.info)
+        
 tutor1 = tutor("mark small",22,"001")
 tutor2 = tutor("ren batts",21,"002")
 student1 = student("will breks", 19,"003")
@@ -71,3 +80,4 @@ course1 = course("Physics of the Human Body")
 course2 = course("Web Development II")
 session1 = createsession("s001","course1","5pm","feb21","room1","tutor1","Available")
 sessionpick1 = picksession("s001","course1","5pm","feb21","room1","tutor1","student1")
+review1 = review("r001","12pm","feb22","heron chambers","The session was just wonderful")
